5274d61cdbd0bce68130298d40393837baf07091
marubontan <marubontan@gmail.com>
2023-10-02 19:08:51 -0300

    fix version

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 
-from dcraft.__version__ import VERSION
-
 deps = ["pandas", "pyarrow"]
 gcp_deps = ["google-cloud-storage>=2.6.0", "google-cloud-bigquery", "pyarrow"]
 test_requires = ["pytest"]
 
 setup(
     name="dcraft",
-    version=VERSION,
+    version="0.0.1",
     packages=["dcraft"],
     install_requires=deps,
     test_requires=test_requires,
