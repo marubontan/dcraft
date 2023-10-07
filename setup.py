d7dcb86628e9e9619cbe345288bd91af818703b1
marubontan <marubontan@gmail.com>
2023-10-07 20:47:18 -0300

    fix docs requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ test_requires = ["pytest"]
 
 setup(
     name="dcraft",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(exclude=["tests"]),
     install_requires=deps,
     test_requires=test_requires,
