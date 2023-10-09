b913458490556156de6c46b2913ccabf0eaa01a0
Shuhei Kishi <marubontan@gmail.com>
2023-10-09 07:23:22 -0300

    add validation test (#19)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ test_requires = ["pytest"]
 
 setup(
     name="dcraft",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests"]),
     install_requires=deps,
     test_requires=test_requires,
