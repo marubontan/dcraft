94f581f6a44633d075108da8bdcf2c7cd9866f9f
Shuhei Kishi <marubontan@gmail.com>
2023-10-12 08:18:33 -0300

    add dict list save and load case (#23)

--- test/integration/interface/data/test_gcs.py
+++ test/integration/interface/data/test_gcs.py
@@ -25,7 +25,31 @@ def test_init():
 
 
 @mark.integration
-def test_save_dict():
+def test_save_dict_json():
+    data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
+    content = [{"a": 2, "b": 2}, {"a": 2, "b": 2}]
+    data_repository.save(
+        content, "test-project", "raw", "test-id", "json", ContentType.DICT
+    )
+    client = Client(GCP_PROJECT)
+    bucket = client.get_bucket(GCS_BUCKET)
+    blob = bucket.blob("test-project/raw/test-id.json")
+    saved_content = blob.download_as_text()
+    assert json.loads(saved_content) == content
+
+
+@mark.integration
+def test_load_dict_json():
+    data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
+    loaded_content = data_repository.load(
+        "test-project", "raw", "test-id", "json", ContentType.DICT
+    )
+    expected_content = [{"a": 2, "b": 2}, {"a": 2, "b": 2}]
+    assert loaded_content == expected_content
+
+
+@mark.integration
+def test_save_dict_list_json():
     data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
     content = {"a": 2, "b": 2}
     data_repository.save(
@@ -39,7 +63,7 @@ def test_save_dict():
 
 
 @mark.integration
-def test_load_dict():
+def test_load_dict_list_json():
     data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
     loaded_content = data_repository.load(
         "test-project", "raw", "test-id", "json", ContentType.DICT
@@ -49,7 +73,7 @@ def test_load_dict():
 
 
 @mark.integration
-def test_save_csv():
+def test_save_df_csv():
     data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
     content = pd.DataFrame({"a": [1], "b": [2]})
     data_repository.save(
@@ -63,7 +87,7 @@ def test_save_csv():
 
 
 @mark.integration
-def test_load_csv():
+def test_load_df_csv():
     data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
     loaded_content = data_repository.load(
         "test-project", "raw", "test-id", "csv", ContentType.DF
@@ -73,7 +97,7 @@ def test_load_csv():
 
 
 @mark.integration
-def test_save_parquet():
+def test_save_df_parquet():
     data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
     content = pd.DataFrame({"a": [1], "b": [2]})
     data_repository.save(
@@ -87,7 +111,7 @@ def test_save_parquet():
 
 
 @mark.integration
-def test_load_parquet():
+def test_load_df_parquet():
     data_repository = GcsDataRepository(GCP_PROJECT, GCS_BUCKET)
     loaded_content = data_repository.load(
         "test-project", "raw", "test-id", "parquet", ContentType.DF
