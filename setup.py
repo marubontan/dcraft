77ba19c50d5e887f12d5fcd0e897e2a8eb66e99f
marubontan <marubontan@gmail.com>
2023-10-19 23:00:13 -0300

    add requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 deps = ["pandas", "pyarrow"]
 gcp_deps = ["google-cloud-storage>=2.6.0", "google-cloud-bigquery", "pyarrow"]
 mongo_deps = ["pymongo"]
+minio_deps = ["minio"]
 test_requires = ["pytest"]
 
 setup(
@@ -13,6 +14,11 @@ setup(
     test_requires=test_requires,
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    extras_require={"gcp": gcp_deps, "mongo": mongo_deps, "test": test_requires},
+    extras_require={
+        "gcp": gcp_deps,
+        "mongo": mongo_deps,
+        "minio": minio_deps,
+        "test": test_requires,
+    },
     author="Shuhei Kishi",
 )
