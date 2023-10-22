7f60540c469adda6bcdf72cb7151dc7a6b115c26
marubontan <marubontan@gmail.com>
2023-10-22 17:27:12 -0300

    fix readme loading

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
 from setuptools import find_packages, setup
+
+README_PATH = os.path.join(os.path.dirname(__file__), "README.md")
 
 deps = ["pandas", "pyarrow"]
 gcp_deps = ["google-cloud-storage>=2.6.0", "google-cloud-bigquery", "pyarrow"]
@@ -13,7 +16,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     install_requires=deps,
     test_requires=test_requires,
-    long_description=open("README.md").read(),
+    long_description=open(README_PATH).read(),
     long_description_content_type="text/markdown",
     extras_require={
         "gcp": gcp_deps,
